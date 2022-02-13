a06d2a245da3aeae7a124364e5d4daec180cb795
User <ew.suuper@yandex.ru>
2022-02-13 22:49:30 +0600

    hh

--- movie_app/models.py
+++ movie_app/models.py
@@ -1,22 +1,26 @@
 from django.db import models
 
+
 class Director(models.Model):
     name = models.TextField(max_length=20)
 
     def __str__(self):
         return self.name
 
+
 class Movie(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField(null=True,blank=True)
+    description = models.TextField(null=True, blank=True)
     duration = models.DurationField()
-    director = models.ForeignKey(Director,on_delete=models.CASCADE)
+    director = models.ForeignKey(Director, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.title
 
+
 class Review(models.Model):
     text = models.CharField(max_length=100)
-    movie = models.ForeignKey(Movie,on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.text
-
