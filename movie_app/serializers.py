78d5ca825d83c39536db55a1a6e1c34276050384
User <ew.suuper@yandex.ru>
2022-02-13 22:52:14 +0600

    hhkk

--- movie_app/serializers.py
+++ movie_app/serializers.py
@@ -1,17 +1,20 @@
-from  rest_framework import serializers
-from  movie_app.models import Director
-from  movie_app.models import Movie
-from  movie_app.models import Review
+from rest_framework import serializers
+from movie_app.models import Director
+from movie_app.models import Movie
+from movie_app.models import Review
+
 
 class NameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Director
         fields = '__all__'
 
+
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
+
 
 class RSerializer(serializers.ModelSerializer):
     class Meta:
